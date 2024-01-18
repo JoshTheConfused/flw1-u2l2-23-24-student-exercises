8126c5cb465b1ef41858145521964e819d14351f
JoshTheConfused <97866996+JoshTheConfused@users.noreply.github.com>
2024-01-18 21:51:04 +0000

    All work done during fellowship

--- arrow.js
+++ arrow.js
@@ -1,24 +1,23 @@
 // 1. Covert to an arrow function.
-function sayHello() {
-    console.log("Hello, world!");
-  }
+const sayHello = () => {
+  console.log("Hello World!");
+}
 
 
 // 2. Convert to an arrow function.
-function square(num) {
-    return num * num;
-  }
+const square = (num) => {
+  return num * num;
+}
   
 
 // 3. Convert to an arrow function.
-function addNums(a, b) {
-    return a + b;
-  }
-  
+const addNums = (a, b) => {
+  return a + b;
+} 
 
 
-console.log(sayHello())  // should print "Hello, world!"
-console.log(square(2)) // should print 4
-console.log(addNums(6, 4)) // should print 10
+console.log(sayHello());  // should print "Hello, world!"
+console.log(square(2)); // should print 4
+console.log(addNums(6, 4)); // should print 10
   
   
--- dogCalculator.js
+++ dogCalculator.js
@@ -5,15 +5,13 @@ let result = document.querySelector(".result");
 
 
 // 1. Convert to an arrow function.
-function calculateDoggieAge(humanAge) {
+const calculateDoggieAge = (humanAge) => {
 
   // 2. Declare a doggieAge variable and set it equal to the humanAge parameter multiplied by 7.
-  
+  doggieAge = humanAge / 7;
 
   // 3. Update the result div's inner html to display the dog's age. Use string interpolation to create the message: "Your doggie is _________ years old in human years!"
-
-  
-  
+  result.innerHTML = `Your doggie is ${doggieAge} years old in dog years!`
 }
 
 
@@ -21,10 +19,10 @@ function calculateDoggieAge(humanAge) {
 equalsButton.addEventListener("click", () => {
 
   // 4. Declare a userInput variable and set it equal to the value of the input. This needs to be a number, not a string! Be sure to place that inside our Number() method!
-  
+  userInput = Number(input.value);
 
   // 5. Call the calculateDoggieAge function. Be sure to include the userInput variable as the parameter!
-
+  calculateDoggieAge(userInput);
 
 })
 
